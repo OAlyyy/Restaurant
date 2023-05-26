37ba42345ac826ac419416005e00ea345c4ddb90
OAlyyy <omar_mahfouz2000@yahoo.com>
2023-05-26 21:15:58 +0200

    should be working

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const app = express();
-const cors = require('cors');
+const functions = require("firebase-functions");
 
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -9,39 +10,37 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./models");
 
-
 // Routers
+const productRouter = require("./routes/Product");
+app.use("/product", productRouter);
 
+const userRouter = require("./routes/User");
+app.use("/user/", userRouter);
 
-const productRouter= require('./routes/Product')
-app.use("/product",productRouter);
+const ordersRouter = require("./routes/Orders");
+app.use("/orders/", ordersRouter);
 
-const userRouter = require('./routes/User');
-app.use('/user/', userRouter);
-
-
-const ordersRouter = require('./routes/Orders');
-app.use('/orders/', ordersRouter);
-
-
-const paymentRouter= require('./routes/Payment')
+const paymentRouter = require("./routes/Payment");
 app.use("/payment", paymentRouter);
-
-
 
 // End Routers
 
 
+exports.apiHandler = functions.https.onRequest(app);
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => { 
+
+
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(3001, () => {
       console.log("Server running on port 3001");
-      });  
-  }).catch((e)=>{
-      console.log(e);
+    });
   })
+  .catch((e) => {
+    console.log(e);
+  });
 
-
-  // app.get("/", (req,res) => { 
-  //   res.json({message : "Welcome to the Future of Ordering"});
-  //   });  
+// app.get("/", (req,res) => {
+//   res.json({message : "Welcome to the Future of Ordering"});
+//   });
