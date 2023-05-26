15cef3141ab5da2b1eca15c3e795a4b650f705dd
OAlyyy <omar_mahfouz2000@yahoo.com>
2023-05-26 19:11:35 +0200

    dependecies conflict working

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
@@ -12,28 +12,20 @@ import CssBaseline from '@mui/material/CssBaseline';
 const theme = createTheme();
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-
     <ThemeProvider theme={theme}>
-    <CssBaseline />
-
-    <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={ <App /> }>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </Provider>
+      <CssBaseline />
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
-
-
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
