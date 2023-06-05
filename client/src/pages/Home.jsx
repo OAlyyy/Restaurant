375a6257ecb6757d7fe9eb91bebc7733c56abe8d
OAlyyy <omar_mahfouz2000@yahoo.com>
2023-06-05 20:30:01 +0200

    finalizing css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Grid } from "@mui/material";
 
 const Home = () => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={0}>
       <Grid item xs={12}>
         <Banner />
       </Grid>
